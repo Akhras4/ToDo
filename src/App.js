4fc8873e7fe1571b3ea9879c2bdb7f074302c99e
Akhras <65845301+Akhras4@users.noreply.github.com>
2024-04-03 18:55:56 +0200

    Quotes Generator

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Nav from'./components/nav';
 import List from './components/list';
+import Quotes from './components/Quotes';
 import './App.css';
 function App() {
   return (
     <div className="App">
       <Nav />
       <List />
+      <Quotes />
     </div>
   );
 }
